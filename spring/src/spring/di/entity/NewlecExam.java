848a821ae512526b7ee05b90d5902b37fa024194
dltmfekgml <dltmfekgml@naver.com>
2023-03-09 17:56:15 +0900

    생성자 객체주입

--- spring/src/spring/di/entity/NewlecExam.java
+++ spring/src/spring/di/entity/NewlecExam.java
@@ -6,7 +6,19 @@ public class NewlecExam implements Exam {
 	private int math;
 	private int com;
 	
-	@Override
+	 public NewlecExam() {
+        // TODO Auto-generated constructor stub
+    }
+	 
+	public NewlecExam( int kor, int eng, int math, int com ) {
+        this.kor = kor;
+        this.eng = eng;
+        this.math = math;
+        this.com = com;
+    }
+
+
+    @Override
 	public int total() {
 		// TODO Auto-generated method stub
 		return kor+eng+math+com;
@@ -17,5 +29,18 @@ public class NewlecExam implements Exam {
 		// TODO Auto-generated method stub
 		return total() / 4.0f;
 	}
+
+    @Override
+    public String toString() {
+        return "NewlecExam [kor=" + kor + ", eng=" + eng + ", math=" + math + ", com=" + com + "]";
+    }
+
+    public void setKor( int kor ) { this.kor = kor; }
+
+    public void setEng( int eng ) { this.eng = eng; } 
+
+    public void setMath( int math ) { this.math = math; }
+
+    public void setCom( int com ) { this.com = com; }
 
 }
